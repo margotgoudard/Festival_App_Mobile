83d85c97bff9820112c094850ea71f97b04408d6
margotgoudard <goudard.margot@hotmail.com>
2024-03-14 21:48:48 +0100

    bug regle

--- festivalAPP/Model/Notification.swift
+++ festivalAPP/Model/Notification.swift
@@ -6,23 +6,3 @@
 //
 
 import Foundation
-import SwiftUI
-
-struct Notification: Codable, Identifiable, Hashable {
-    var id: Int {
-            get { idnotification }
-            set { idnotification = newValue }
-        }
-    var idnotification: Int
-    var label: String
-    var iduser: Int
-    var idfestival: Int
-    
-    
-    enum CodingKeys: String, CodingKey {
-        case idnotification = "idnotification"
-        case label
-        case iduser
-        case idfestival
-    }
-}
