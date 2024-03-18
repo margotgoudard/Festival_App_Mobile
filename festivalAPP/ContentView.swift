01b15f6573c8b080f3032693a3e18a6423054a1c
margotgoudard <goudard.margot@hotmail.com>
2024-03-18 12:09:30 +0100

    page profil

--- festivalAPP/ContentView.swift
+++ festivalAPP/ContentView.swift
@@ -1,141 +1,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var mail: String = ""
-    @State private var password: String = ""
-    @State private var notification: String = ""
-    @State private var isAuthenticated = false
     @State private var isRegistering = false
-    @State private var userData: User?
+    @State private var isLoggingIn = false
 
     var body: some View {
         NavigationView {
-            VStack(spacing: 20) {
-        
-                TextField("Email", text: $mail)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .keyboardType(.emailAddress)
-                    .autocapitalization(.none)
-                    .padding(.horizontal, 30)
-                    .cornerRadius(10)
-                    .shadow(radius: 2)
+            VStack {
+                Spacer()
+                Image(uiImage: UIImage(named: "logo.png")!)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                Text("Festival du Jeu de Montpellier")
+                    .font(.title)
+                    .padding(.bottom, 20)
                 
-                SecureField("Mot de passe", text: $password)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.horizontal, 30)
-                    .cornerRadius(10)
-                    .shadow(radius: 2)
-                
-                if !notification.isEmpty {
-                    Text(notification)
-                        .foregroundColor(.red)
-                }
-                
-                Button("Se connecter") {
-                    login()
+                Button("Rejoignez-nous dès maintenant") {
+                    isRegistering = true
                 }
                 .buttonStyle(PrimaryButtonStyle())
-                
-                let festivalUtils = FestivalUtils()
-                
-                if let user = userData {
-                    NavigationLink(destination: ProfilView(festivalUtils: festivalUtils, user: user), isActive:.constant(true)) { EmptyView() }
-                }
-                Button("S'inscrire") {
-                    inscrire()
-                }
+                .padding(.bottom, 10)
                 
                 NavigationLink(destination: InscrireView(), isActive: $isRegistering) { EmptyView() }
-                .buttonStyle(PrimaryButtonStyle())
+
+                Spacer()
             }
-        }.navigationBarBackButtonHidden(true)
-
-    }
-    
-    
-    func inscrire() {
-        isRegistering = true
-    }
-
-    
-    func login() {
-        print("Début de la méthode login")
-        
-        guard !mail.isEmpty, !password.isEmpty else {
-            notification = "Veuillez remplir tous les champs."
-            print("Email ou mot de passe vide")
-            return
-        }
-        
-        guard let url = URL(string: "https://benevole-app-back.onrender.com/user/connexion") else {
-            print("L'URL est invalide")
-            return
-        }
-        
-        let body: [String: String] = ["mail": mail, "mdp": password]
-        guard let finalBody = try? JSONEncoder().encode(body) else {
-            print("Échec de l'encodage du body")
-            return
-        }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.httpBody = finalBody
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        print("Envoi de la requête")
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            DispatchQueue.main.async {
-                if let error = error {
-                    notification = "Network request failed"
-                    print("Échec de la requête réseau : \(error.localizedDescription)")
-                    return
-                }
-                
-                guard let data = data else {
-                    notification = "Aucune donnée reçue"
-                    print("Aucune donnée reçue du serveur")
-                    return
-                }
-                
-                if let httpResponse = response as? HTTPURLResponse {
-                    print("Réponse du serveur : \(httpResponse.statusCode)")
-                }
-                do {
-                    let loginResponse = try JSONDecoder().decode(LoginResponse.self, from: data)
-                    if loginResponse.auth {
-                        UserDefaults.standard.set(loginResponse.token, forKey: "token")
-                        UserDefaults.standard.set(loginResponse.user?.iduser, forKey: "iduser")
-
-                        userData = loginResponse.user
-                        isAuthenticated = true
-                        self.userData = loginResponse.user
-                        print("Connexion réussie")
-                        print(loginResponse.user)
-                    } else {
-                        notification = "Login failed"
-                        print("Échec de la connexion : auth = false")
-                    }
-                } catch {
-                    notification = "Login failed"
-                    print("Erreur de décodage : \(error.localizedDescription)")
-                }
+            .navigationBarItems(trailing: Button(action: {
+                isLoggingIn = true
+            }) {
+                Text("Se connecter")
+            })
+            .fullScreenCover(isPresented: $isLoggingIn) {
+                LoginView() // Assume LoginView is your login component
             }
-        }.resume()
-    }
-}
-
-
-
-struct PrimaryButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .padding()
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            .scaleEffect(configuration.isPressed ? 0.95 : 1)
+        }
     }
 }
